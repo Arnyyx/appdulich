5199f78970e3ba1b6090e6adcd66196872c434c2
Arny <tucvbn@gmail.com>
2023-09-22 21:47:31 +0700

    Chuyến đi

--- app/src/main/java/com/example/teamcht/VanChuyen/DanhSachChuyenDi.java
+++ app/src/main/java/com/example/teamcht/VanChuyen/DanhSachChuyenDi.java
@@ -1,6 +1,5 @@
 package com.example.teamcht.VanChuyen;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -8,15 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.teamcht.R;
 
-public class VanChuyen extends AppCompatActivity {
+public class DanhSachChuyenDi extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vanchuyen);
-
-        findViewById(R.id.btnTimChuyenDi).setOnClickListener(view -> {
-            Intent intent = new Intent(this, DanhSachChuyenDi.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.vanchuyen_dschuyendi);
     }
 }
